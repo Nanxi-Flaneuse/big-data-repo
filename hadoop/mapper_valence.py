f1d36e1c697622ac97cff7250cfdc4b6b0cb6a4a
Nanxi-13 <selenananseki@gmail.com>
2024-10-14 12:06:01 -0400

    new changes

---
+++ hadoop/mapper_valence.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import sys, re
+import random
+from valence import clean_text, get_word_valence
+import fileinput
+import os
+
+
+
+def main(argv):
+    # for line in fileinput.input():
+    #     if fileinput.isfirstline():
+    #         filename = fileinput.filename()
+    #         print("Filename:", filename)
+    file = os.environ['mapreduce_map_input_file']
+    prez_name = file[:-17]
+    line = sys.stdin.readline()
+    # pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
+    try:
+        while line:
+            # print(pattern.findall(line))
+            line = clean_text(line)
+            # print(line)
+            for word in line:
+                print(prez_name + "\t" + str(get_word_valence(word)))
+            line = sys.stdin.readline()
+    except EOFError as error:
+        return None
+
+if __name__ == "__main__":
+    main(sys.argv)
