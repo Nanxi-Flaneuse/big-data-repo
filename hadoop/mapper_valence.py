efafd737e0a37c8e776bba355f004c379d724832
Nanxi-13 <selenananseki@gmail.com>
2024-10-15 11:37:21 -0400

    small test

--- hadoop/mapper_valence.py
+++ hadoop/mapper_valence.py
@@ -5,24 +5,23 @@ from valence import clean_text, get_word_valence, get_sent_dict
 import fileinput
 import os
 
-
+# is there something wrong with how I'm reading the file
+# to precess all the prz speeches in hadoop, can I use tar files instead of untarring all of the tar files?
 
 def main(argv):
 
     ##### to be uncommented ###########
-    file = os.environ['mapreduce_map_input_file']
-    prez_name = file[:-17]
+    # file = os.environ['mapreduce_map_input_file']
+    # prez_name = file[:-17]
     line = sys.stdin.readline()
     # pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
     try:
         while line:
-            # print(pattern.findall(line))
             line = clean_text(line).split()
-            # print(line)
             for word in line:
-                # print('adams' + "\t" + str(get_word_valence(word)))
+                print('adams' + "\t" + str(get_word_valence(word)))
                 ##### to be uncommented ###########
-                print(prez_name + "\t" + str(get_word_valence(word)))
+                # print(prez_name + "\t" + str(get_word_valence(word)))
             line = sys.stdin.readline()
     except EOFError as error:
         return None
