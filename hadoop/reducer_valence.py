fbf43c8d04c74053ec29469e62496b11b1f4b0bb
Nanxi-13 <selenananseki@gmail.com>
2024-10-15 15:06:58 -0400

    testing

--- hadoop/reducer_valence.py
+++ hadoop/reducer_valence.py
@@ -6,6 +6,7 @@ import sys
 
 current_word = None
 # prez_freq = 0
+current_valence = 0
 current_count = 0
 word = None
 
@@ -16,11 +17,12 @@ for line in sys.stdin:
     # print('printing out line')
     # print(line)
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
+    word, valence = line.split('\t', 1)
+    count = 1
 
     # convert count (currently a string) to int
     try:
-        count = int(count)
+        valence = int(valence)
     except ValueError:
         # count was not a number, so silently
         # ignore/discard this line
@@ -29,15 +31,17 @@ for line in sys.stdin:
     # by key (here: word) before it is passed to the reducer
     if current_word == word:
         current_count += count
+        current_valence += valence
         # prez_freq += 1
     else:
         if current_word:
             # write result to STDOUT
-            print ('%s\t%s' % (current_word, current_count))
+            print ('%s\t%s' % (current_word, current_valence/current_count))
         current_count = count
         current_word = word
+        current_valence = 0
         # prez_freq = 1
 
 # do not forget to output the last word if needed!
 if current_word == word:
-    print ('%s\t%s' % (current_word, current_count))
+    print ('%s\t%s' % (current_word, current_valence/current_count))
